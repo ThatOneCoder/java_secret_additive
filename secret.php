69ba59fbce7289e31d613b2adfe5f6a3b702b266
Wynshaft <wynshaft@live.com>
2014-08-02 00:43:37 -0400

    Notes Update
    changed java to php in the notes

--- secret.php
+++ secret.php
@@ -5,7 +5,7 @@
  *
  * Problem:
  * You are given a function 'secret()' that accepts a single integer parameter
- * and returns an integer. In Java, write a program that determines if this function
+ * and returns an integer. In PHP, write a program that determines if this function
  * is an additive function [ secret(x+y) = secret(x) + secret(y) ] for all prime numbers
  * under N where N is a given integer.
  *
